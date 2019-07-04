c68c3ee97370bc6c28b3bc8955ebc922833b1880
azurre <azrr.mail@gmail.com>
2019-07-05 00:00:39 +0300

    Code refactoring

--- README.md
+++ README.md
@@ -1,28 +1,100 @@
-# A convenient component of PHP CLI wrapper
+# PHP Shell  [![Latest Version](https://img.shields.io/github/release/azurre/php-shell.svg?style=flat-square)](https://github.com/azurre/php-shell/releases)
+This small and easy to use PHP shell make it able to run commands as non blocking asynchronous jobs 
 
-# Simple usage
-
-```php
-use Azurre\Component\Shell\Wrapper as Shell;
-
-$cmd = 'ls -lwa /tmp';
-echo "Shell::sCmd: " . Shell::sCmd($cmd) . PHP_EOL;
-echo "Shell::cmd: " . PHP_EOL;
-print_r( Shell::cmd($cmd) );
+## Installation
+Install composer in your project:
+```
+curl -s https://getcomposer.org/installer | php
 ```
 
-# Background tasks
+Require the package with composer:
+```
+composer require azurre/php-shell
+```
+
+## Usage
+
+### Simple example
+
+```php
+$loader = require_once __DIR__ . '/vendor/autoload.php';
+
+use Azurre\Component\System\Shell;
+
+$cmd = 'ls -lwa /tmp';
+echo Shell::create()->run($cmd);
+```
+
+Output
+```
+total 180
+drwxrwxrwt 11 root     root     4096 Jul  4 23:21 .
+drwxr-xr-x 22 root     root     4096 Jun 10 16:23 ..
+drwxrwxrwt  2 root     root     4096 Jun 10 16:23 .ICE-unix
+drwxrwxrwt  2 root     root     4096 Jun 10 16:23 .X11-unix
+drwxrwxrwt  2 root     root     4096 Jun 10 16:23 .XIM-unix
+drwxrwxrwt  2 root     root     4096 Jun 10 16:23 .font-unix
+-rw-------  1 www-data www-data    0 Jul  1 21:21 239315d1a4f341c8b52.14168329mUCCHL
+-rw-r--r--  1 www-data www-data 6468 Jul  1 21:18 416205d1a4e5fe8e166.51692428EFpYWQ
+-rw-r--r--  1 www-data www-data 6469 Jul  1 21:21 443675d1a4f3ce6efd0.08860101uETZWS
+-rw-r--r--  1 www-data www-data 6416 Jul  1 21:26 494485d1a503c71cfd8.3817705715MVk4
+-rw-r--r--  1 www-data www-data 6468 Jul  1 21:19 519335d1a4eba5a2ef8.14398347CWkGkb
+-rw-------  1 www-data www-data    0 Jul  1 21:27 575735d1a508060a080.18275551cZg15T
+```
+
+### Background tasks
 
 ```PHP
-$Shell = Shell::init()
-        ->setOutput('/tmp/shell.log')
-        ->setErrorOutput('/tmp/shell.log')
-        ->setAsync()
-        ->exec('ping -c 10 127.0.0.1');
+$shell = new Shell;
+$shell->runAsync('ping -c 5 127.0.0.1');
 
 echo "Start background task\n";
-while($Shell->isProcessRunning()) {
+while($shell->isProcessRunning()) {
     echo 'Working...' .PHP_EOL;
     sleep(1);
 }
+echo $shell;
 ```
+
+Output
+```
+Start background task
+Working...
+Working...
+Working...
+Working...
+Working...
+PING 127.0.0.1 (127.0.0.1) 56(84) bytes of data.
+64 bytes from 127.0.0.1: icmp_seq=1 ttl=64 time=0.020 ms
+64 bytes from 127.0.0.1: icmp_seq=2 ttl=64 time=0.022 ms
+64 bytes from 127.0.0.1: icmp_seq=3 ttl=64 time=0.015 ms
+64 bytes from 127.0.0.1: icmp_seq=4 ttl=64 time=0.023 ms
+64 bytes from 127.0.0.1: icmp_seq=5 ttl=64 time=0.019 ms
+
+--- 127.0.0.1 ping statistics ---
+5 packets transmitted, 5 received, 0% packet loss, time 4099ms
+rtt min/avg/max/mdev = 0.015/0.019/0.023/0.006 ms
+```
+
+### Error handling
+
+```PHP
+$shell = new Shell;
+$shell->run('ls /A/1/2/3/4')->waitForProcess();
+if ($shell->getExitCode() !== Shell::NO_ERROR) {
+    echo "\nExit code: {$shell->getExitCode()}\n";
+    echo "Error: {$shell->getStdError()}\n";
+} else {
+    echo $shell->getStdOut();
+}
+```
+
+Output
+```
+Exit code: 2
+Error: ls: cannot access '/A/1/2/3/4': No such file or directory
+
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/Shell.php
@@ -0,0 +1,357 @@
+<?php
+/**
+ * @author: Alex Milenin
+ * @email admin@azrr.info
+ */
+
+namespace Azurre\Component\System;
+
+/**
+ * Class Shell
+ */
+class Shell
+{
+    /** No error exit code */
+    const NO_ERROR = 0;
+
+    const STDIN  = 0;
+    const STDOUT = 1;
+    const STDERR = 2;
+
+    /** @var string Bash path */
+    const BASH = 'bash';
+
+    /** @var bool run process in background */
+    protected $isAsync;
+
+    /** @var string */
+    protected $cmd;
+
+    /** @var string The initial working dir for the command. Default - the working dir of the current PHP process. */
+    protected $cwd;
+
+    /** @var string STDERR content will be here */
+    protected $stdError;
+
+    /** @var string STDOUT content will be here */
+    protected $stdOut;
+
+    /** @var int Result code will be here after execution */
+    protected $exitCode;
+
+    /** @var int Process ID of current process */
+    protected $pid;
+
+    /** @var bool */
+    protected $isInProgress;
+
+    /** @var resource|null Resource representing the process */
+    protected $process;
+
+    /** @var array */
+    protected $pipes = [];
+
+    /** @var float */
+    protected $checkTimeout = 0.1;
+
+    /** @var array An array with the environment variables for the command that will be run */
+    protected $env = [];
+
+    /**
+     * Run command
+     *
+     * @param string $cmd
+     * @param bool $async
+     * @return $this
+     */
+    public function run($cmd, $async = false)
+    {
+        $this->reset();
+        $descriptors = [
+            static::STDIN => ['pipe', 'r'],
+            static::STDOUT => ['pipe', 'w'],
+            static::STDERR => ['pipe', 'w']
+        ];
+        $this->cmd = $cmd;
+        $this->isAsync = $async;
+        $this->isInProgress = true;
+        $this->process = proc_open(static::BASH, $descriptors, $this->pipes, $this->getCwd(), $this->getEnv());
+        if (!$this->process) {
+            return $this;
+        }
+        fwrite($this->pipes[static::STDIN], $cmd);
+        fclose($this->pipes[static::STDIN]);
+        if ($async) {
+            stream_set_blocking($this->pipes[static::STDOUT], false);
+            stream_set_blocking($this->pipes[static::STDERR], false);
+        } else {
+            $this->stdOut = stream_get_contents($this->pipes[static::STDOUT]);
+            $this->stdError = stream_get_contents($this->pipes[static::STDERR]);
+            $this->exitCode = proc_close($this->process);
+            $this->isInProgress = false;
+        }
+        return $this;
+    }
+
+    /**
+     * Run command
+     *
+     * @param string $cmd
+     * @return $this
+     */
+    public function runAsync($cmd)
+    {
+        return $this->run($cmd, true);
+    }
+
+    /**
+     * Reset properties
+     *
+     * @return $this
+     */
+    protected function reset()
+    {
+        $this->isInProgress = false;
+        $this->isAsync = false;
+        $this->stdError = null;
+        $this->stdOut = null;
+        $this->exitCode = null;
+        $this->cmd = null;
+        $this->pid = null;
+
+        return $this;
+    }
+
+    /**
+     * Is process running asynchronous
+     *
+     * @return bool
+     */
+    public function isAsync()
+    {
+        return $this->isAsync;
+    }
+
+    /**
+     * Get exit code
+     *
+     * @return int|null
+     */
+    public function getExitCode()
+    {
+        if ($this->isInProgress) {
+            return null;
+        }
+        if (!$this->exitCode) {
+            $this->getProcessStatus();
+        }
+        return $this->exitCode;
+    }
+
+    /**
+     * Get PID of executed command
+     *
+     * @return int|null
+     */
+    public function getPid()
+    {
+        return $this->pid;
+    }
+
+    /**
+     * Get last command
+     *
+     * @return string
+     */
+    public function getCmd()
+    {
+        return $this->cmd;
+    }
+
+    /**
+     * Get environment variables
+     *
+     * @return array
+     */
+    public function getEnv()
+    {
+        return $this->env;
+    }
+
+    /**
+     * Set environment variables
+     *
+     * @param array $env
+     * @return $this
+     */
+    public function setEnv(array $env)
+    {
+        $this->env = $env;
+        return $this;
+    }
+
+    /**
+     * Get initial working dir
+     *
+     * @return string
+     */
+    public function getCwd()
+    {
+        return $this->cwd;
+    }
+
+    /**
+     * Set initial working dir
+     *
+     * @param string $cwd
+     * @return $this
+     */
+    public function setCwd($cwd)
+    {
+        $this->cwd = $cwd;
+        return $this;
+    }
+
+    /**
+     * Get errors output of executed command
+     *
+     * @return string|null
+     */
+    public function getStdError()
+    {
+        if (is_resource($this->pipes[static::STDERR]) && $this->isProcessRunning()) {
+            $this->stdError .= stream_get_contents($this->pipes[static::STDERR]);
+        }
+        return $this->stdError;
+    }
+
+    /**
+     * Get output of executed command
+     *
+     * @return string
+     */
+    public function getStdOut()
+    {
+        if (is_resource($this->pipes[static::STDOUT]) && $this->isProcessRunning()) {
+            $this->stdOut .= stream_get_contents($this->pipes[static::STDOUT]);
+        }
+        return $this->stdOut;
+    }
+
+    /**
+     * @return float
+     */
+    public function getCheckTimeout()
+    {
+        return $this->checkTimeout;
+    }
+
+    /**
+     * @param float $checkTimeout
+     * @return $this
+     */
+    public function setCheckTimeout($checkTimeout)
+    {
+        $this->checkTimeout = $checkTimeout * 1;
+        return $this;
+    }
+
+    /**
+     * @param int $pid
+     * @return string
+     */
+    public function getProcessInfo($pid = null)
+    {
+        $pid = (int)$pid;
+        $path = "/proc/{$pid}/status";
+        clearstatcache();
+        return is_file($path) ? file_get_contents($path) : '';
+    }
+
+    /**
+     * @return bool
+     */
+    public function isProcessRunning()
+    {
+        if ($this->isInProgress && $this->isAsync) {
+            $this->getProcessStatus('running');
+            if (!$this->isInProgress) {
+                $this->stdOut .= stream_get_contents($this->pipes[static::STDOUT]);
+                $this->stdError .= stream_get_contents($this->pipes[static::STDERR]);
+            }
+        }
+        return $this->isInProgress;
+    }
+
+    /**
+     * @param string $element
+     * @return array|bool
+     */
+    public function getProcessStatus($element = null)
+    {
+        if ($this->isInProgress && $this->isAsync && is_resource($this->process)) {
+            $status = proc_get_status($this->process);
+            if ($status) {
+                $this->pid = $status['pid'];
+                $this->isInProgress = $status['running'];
+                if (is_numeric($status['exitcode']) && $status['exitcode'] >= 0) {
+                    $this->exitCode = $status['exitcode'];
+                }
+                if ($element && isset($status[$element])) {
+                    return $status[$element];
+                }
+                return $status;
+            }
+            $this->isInProgress = false;
+        }
+        return $this->isInProgress;
+    }
+
+    /**
+     * @return $this
+     */
+    public function waitForProcess()
+    {
+        if ($this->isInProgress && $this->isAsync) {
+            while ($this->isProcessRunning()) {
+                usleep($this->checkTimeout * 1000000);
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public static function create()
+    {
+        return new static;
+    }
+
+    /**
+     * Close the process
+     *
+     * @return $this
+     */
+    public function close()
+    {
+        if (is_resource($this->process)) {
+            proc_close($this->process);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return (string)$this->getStdOut();
+    }
+
+    public function __destruct()
+    {
+        $this->close();
+    }
+}
